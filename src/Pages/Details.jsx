89c57c484db554c434c0959ba792bf7facf20178
Alex Kenty <ak5kenty@gmail.com>
2021-10-20 10:58:41 +0500

    finished

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -1,8 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { IoArrowBack } from "react-icons/io5";
+import { useHistory, useParams } from "react-router";
+import { Button } from "../components/Button";
+import { Info } from "../components/Info";
+import { searchByCountry } from "../config";
 
-export const Details = ({match}) => {
+export const Details = () => {
+	const [details, setDetails] = useState([]);
+	const {name} = useParams();
+	const {goBack, push} = useHistory();
+
+	useEffect(() => {
+		axios.get(searchByCountry(name)).then(resp => setDetails(resp.data[0]));
+		//eslint-disable-next-line
+	},[name])
+
+	
 	return (
 		<div>
-			Details of 
+			<Button onClick={goBack}>
+				<IoArrowBack/>&nbsp;Back
+			</Button>
+			<Info push={push} {...details}/>
 		</div>
 	)
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export const Button = styled.button`
+	display: flex;
+	align-items: center;
+	box-shadow: var(--shadow);
+	border-radius: var(--radii);
+	padding: 5px 10px;
+	cursor: pointer;
+	border: none;
+	background-color: var(--colors-ui-base);
+`;
